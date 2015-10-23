dd36233893ee4bb72dcfc1b6e68fb8ecc5f2ce82
Ben Sparks <bsparks@unicon.net>
2015-10-23 14:23:43 -0700

    update demo

--- app/game/something.js
+++ app/game/something.js
@@ -4,18 +4,12 @@ import System from '../../ces/system';
 import THREE from 'three';
 
 class SomeSystem extends System {
-    constructor() {
-        super();
-
-        this.test = 0;
-    }
-
     addedToWorld(world) {
         super.addedToWorld(world);
 
         world.onEntityAdded('primitive').add(function (entity) {
             var geometry = new THREE.BoxGeometry(200, 200, 200);
-            var material = new THREE.MeshBasicMaterial({ color: 0xff00ff, wireframe: false });
+            var material = new THREE.MeshPhongMaterial({ color: 0xff00ff, wireframe: false });
             var mesh = new THREE.Mesh(geometry, material);
 
             entity.addComponent('sceneObject', {obj: mesh});
@@ -34,16 +28,6 @@ class SomeSystem extends System {
             rotation.x += 0.001 * dt;
             rotation.z += 0.001 * dt;
         });
-
-        this.world.getFamily('sceneObject', 'transform').forEach(function(entity) {
-            var sceneObject = entity.getComponent('sceneObject').obj;
-            var {position, rotation} = entity.getComponent('transform');
-
-            sceneObject.position.set(position.x, position.y, position.z);
-            sceneObject.rotation.set(rotation.x, rotation.y, rotation.z);
-        });
-
-        this.test++;
     }
 }
 
--- app/main.js
+++ app/main.js
@@ -7,6 +7,8 @@ import {World, Entity, Component} from '../ces/ces';
 import SomeSystem from './game/something';
 import timing from './timing';
 
+import SceneManager from '../graphics/sceneManager';
+
 window.THREE = THREE;
 
 window.foo = new Component({ test: 'abc', bar: 1, twingle: [1, 2, 3] });
@@ -21,31 +23,39 @@ world.scene = scene;
 camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
 camera.position.z = 1000;
 
+var light = new THREE.PointLight(0xffffff, 0.8);
+scene.add(light);
+
+scene.add(new THREE.AmbientLight(0x404040));
+
 renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
 
 world.addSystem(new SomeSystem());
+world.addSystem(new SceneManager(scene));
 
 var e1 = new Entity();
 e1.addComponent('transform', {
     position: new THREE.Vector3(),
     rotation: new THREE.Vector3(),
-    scale: new THREE.Vector3()
+    scale: new THREE.Vector3(1, 1, 1)
 });
 var e2 = new Entity();
 e2.addComponent('transform', {
-    position: new THREE.Vector3(),
+    position: new THREE.Vector3(-300, 0, 0),
     rotation: new THREE.Vector3(),
-    scale: new THREE.Vector3()
+    scale: new THREE.Vector3(1, 1, 1)
 });
+e2.addComponent('primitive');
+e2.addComponent('spins');
 var e3 = new Entity();
 e3.addComponent('ghostId', { id: 'blah' });
 e3.addComponent('transform', {
-    position: new THREE.Vector3(),
+    position: new THREE.Vector3(300, 0, 0),
     rotation: new THREE.Vector3(),
-    scale: new THREE.Vector3()
+    scale: new THREE.Vector3(1, 1, 1)
 });
 e3.addComponent('primitive');
 e3.addComponent('spins');
